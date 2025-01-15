b8518bfe0837c22c9057a524befde595c7f67395
Lesia Hlukhaniuk <olesia.hlukhaniuk@computools.com>
2025-01-15 11:40:40 +0200

    feat(FS-2.2): create footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+import { LayoutComponent } from './layout/layout.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [LayoutComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'food-shop-app';
-}
+export class AppComponent {}
